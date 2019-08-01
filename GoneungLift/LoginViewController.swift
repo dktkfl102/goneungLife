805fe05e6b85e6841e3cc1e1c87d48c1a291d4e8
김소연 <soyeon@blockchainglobal.co.kr>
2019-08-01 22:31:10 +0900

    커밋 테스트

--- GoneungLift/LoginViewController.swift
+++ GoneungLift/LoginViewController.swift
@@ -14,6 +14,7 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var tfPw: UITextField!
     
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
